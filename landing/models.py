21b23122489a5cfef8d3cc5b08d07853389a70e0
BrrunaRibeiro <brrunarib@gmail.com>
2024-08-14 16:14:49 +0200

    Delete Client model(Django's built-in User model will be used instead)

--- landing/models.py
+++ landing/models.py
@@ -3,18 +3,3 @@ from django.contrib.auth.models import User
 
 
 # Create your models here.
-class Client(models.Model):
-    id = models.AutoField(primary_key=True)
-    username = models.CharField(max_length=50, unique=True)
-    email = models.EmailField(unique=True)
-    password = models.CharField(max_length=100)
-    first_name = models.CharField(max_length=50)
-    last_name = models.CharField(max_length=50)
-    phone_number = models.CharField(max_length=20, unique=True)
-    created_at = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return str(self.username)
-
-    def __repr__(self):
-        return f"User(username={self.username!r}, email={self.email!r})"
