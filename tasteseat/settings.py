8159024e5b16dc3f843831e51a9608117efa1357
BrrunaRibeiro <brrunarib@gmail.com>
2024-08-15 17:31:10 +0200

    Add Restaurant's data using fixtures

--- tasteseat/settings.py
+++ tasteseat/settings.py
@@ -19,7 +19,7 @@ if os.path.isfile('env.py'):
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# Ensure the media directory is set in the project's root level
+TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
@@ -68,7 +68,7 @@ ROOT_URLCONF = 'tasteseat.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATES_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
