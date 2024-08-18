d443d7d43514143bb5f447c3ec9b75e72019f2cb
BrrunaRibeiro <brrunarib@gmail.com>
2024-08-18 12:05:16 +0200

    Add a text logo to main.html and add basic style to the header

--- landing/urls.py
+++ landing/urls.py
@@ -3,7 +3,7 @@ from .views import restaurant_detail, book_table, booking_confirmation
 from django.urls import path
 
 urlpatterns = [
-    path('', views.ShowRestaurants.as_view(), name='home'),
+    path('restaurant_list', views.ShowRestaurants.as_view(), name='restaurant_list'),
     path('restaurant/<int:restaurant_id>/',
           views.restaurant_detail, name='restaurant_detail'),
     path('restaurant_details/', book_table, name='book_table'),
