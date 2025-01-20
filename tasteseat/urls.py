e0f9ad2c241c0948352f4434fa49f42a183fb4d5
Bruna Ribeiro <brrunarib@gmail.com>
2025-01-20 19:59:29 +0100

    Add 404 error handling in urls.py

--- tasteseat/urls.py
+++ tasteseat/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from landing.views import custom_404_view
 
 
 urlpatterns = [
@@ -13,3 +14,5 @@ urlpatterns = [
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL,
                           document_root=settings.MEDIA_ROOT)
+
+handler404 = 'landing.views.custom_404_view'
